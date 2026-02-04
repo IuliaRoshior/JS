035d979e2fb2cca295b982d46ef14f4f7fa9ec9e
Iulia Roshior <julieroshior2018@gmail.com>
2026-02-04 12:32:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+// let i = 1;
+// for (; i <= 5; ) {
+//   if (i % 2 == 0) {
+//     console.log(i + 1);
+//   }
+//   else {
+//     console.log(i - 1);
+//   }
+//    i++;
+// }
+
+// i = "Anna";
+// console.log(i);
+
+alert("Этот код выполнен из внешнего файла!");
+console.log("Сообщение в консоли");
